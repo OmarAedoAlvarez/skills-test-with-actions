a0235ff7f4ef5b9b97828d95cfc1e9eed7c89127
Omar Aedo <168685350+OmarAedoAlvarez@users.noreply.github.com>
2025-07-25 01:59:27 +0000

    Add coverage report comment for Mona

--- src/calculations.py
+++ src/calculations.py
@@ -4,6 +4,9 @@ import math
 # Installed Modules
 # - None
 
+#@professortocat, I've run my coverage report.
+#Seems there is some opportunity to increase the test coverage. 🧐
+#What should we do next?
 
 def area_of_circle(radius):
     """Calculate the area of a circle given its radius."""
